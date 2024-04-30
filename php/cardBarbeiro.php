0b3bf813a8db29e939e3fa58cd305914e9a60f90
EUGENIO NEVES SPERA <eugenio.nspera@senacsp.edu.br>
2024-04-30 17:27:18 -0300

    Iniciado barbearia.PHP

---
+++ php/cardBarbeiro.php
@@ -0,0 +1,14 @@
+<?php
+
+//verificando se 
+if(isset($_POST['nome'])&&isset($_POST['senha'])&&isset( $_POST['email'])&&isset($_POST['telefone'])&&isset($_POST['aniversario'])&&isset($_POST['data'])){
+
+$nome = $_POST['nome'];
+$senha = $_POST['senha'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$aniversario = $_POST['aniversario'];
+$dataInicio = $_POST['data'];
+}
+
+?>
